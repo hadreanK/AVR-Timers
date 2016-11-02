07d3a2bdf983d757846b7c8fb55d513cbd1c7794
Adrian Kirn <Adrian@adrian-kirns-macbook-2.local>
2016-11-02 14:03:11 -0700

    Added T1 for atMega328P, and renamed akTimers.cpp and akTimers.h to avrTimers.cpp and avrTimers.h

--- avrTimers.cpp
+++ avrTimers.cpp
@@ -3,14 +3,14 @@ This file is to help the LED_Clock arduino file to control my
 LED display I jacked from that sony clock
 */
 
-#ifndef akTimers_cpp
-#define akTimers_cpp
+#ifndef avrTimers_cpp
+#define avrTimers_cpp
 
 // INCLUDES
 	#include "Arduino.h"
 	
 // Functions
-void atMega2560T3( int prescalar, int outCompareRegister){
+void atMega2560_T3( int prescalar, int outCompareRegister){
 		// Set the type of timer
 		TCCR3A = 0x00; // Want bits 1:0 to be 00, they're WGM3 1:0, and everything else too
 		// Set the prescalar
@@ -32,11 +32,11 @@ void atMega2560T3( int prescalar, int outCompareRegister){
 		// Bits 4:3 are WGM3 3:2, so we want them to be 0b01, b/c WGMn3:0 = 0b0100, table 17-2 page 145,
 		//   because this is CTC mode which clears the counter once it gets to OCR3A
 		// Bits 2:0 are the prescalar, coming from clkSelBit
-		OCR3A = 0x0271; // Want the output compare register to be 625 to get 100Hz
-		// 16 MHz, 16e6/256 = 62500
+		OCR3A = outCompareRegister; // Set the output compare register
 		TIMSK3 = 0b0010; // bits 3:1 enable the output compare C:A respectively match interrupt, 0 is overflow
-}// atMega2560T3
-void atMega2560T4( int prescalar, int outCompareRegister){
+	}// atMega2560_T3
+
+void atMega2560_T4( int prescalar, int outCompareRegister){
 		// Set the type of timer
 		TCCR4A = 0x00; // Want bits 1:0 to be 00, they're WGM3 1:0, and everything else too
 		// Set the prescalar
@@ -58,8 +58,34 @@ void atMega2560T4( int prescalar, int outCompareRegister){
 		// Bits 4:3 are WGM3 3:2, so we want them to be 0b01, b/c WGMn3:0 = 0b0100, table 17-2 page 145,
 		//   because this is CTC mode which clears the counter once it gets to OCR3A
 		// Bits 2:0 are the prescalar, coming from clkSelBit
-		OCR4A = 0x0271; // Want the output compare register to be 625 to get 100Hz
-		// 16 MHz, 16e6/256 = 62500
+		OCR4A = outCompareRegister; // Set the output compare register
 		TIMSK4 = 0b0010; // bits 3:1 enable the output compare C:A respectively match interrupt, 0 is overflow
-}// atMega2560T4
+	}// atMega2560_T4
+
+void atMega328P_T1(int prescalar, int outCompareRegister){
+		// Set the type of timer
+		TCCR1A = 0x00; // Want bits 1:0 to be 00, they're WGM3 1:0, and everything else too
+		// Set the prescalar
+		int clkSelBit;
+		switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
+			case 8:
+				clkSelBit = 0b010;
+			case 64:
+				clkSelBit = 0b011;
+			case 256:
+				clkSelBit = 0b100;
+			case 1024:
+				clkSelBit = 0b101;
+			default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
+				clkSelBit = 0b001;
+			}// switch
+		// set prescalar and other settings
+		TCCR1B = 0x08 + clkSelBit; // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
+		// Bits 4:3 are WGM3 3:2, so we want them to be 0b01, b/c WGMn3:0 = 0b0100, table 20-6 page 171,
+		//   because this is CTC mode which clears the counter once it gets to OCR3A
+		// Bits 2:0 are the prescalar, coming from clkSelBit
+		OCR1A = outCompareRegister; // set the output compare register
+		TIMSK1 = 0b0010; // bits 3:1 enable the output compare C:A respectively match interrupt, 0 is overflow
+	} // atMega328P_T1
+
 #endif
--- avrTimers.h
+++ avrTimers.h
@@ -1,26 +1,36 @@
 					// INCLUDES and DEFINES
-#ifndef akTimers_h
-#define akTimers_h
+#ifndef avrTimers_h
+#define avrTimers_h
 
 					// NEW DATA TYPES
 
 					// FUNCTION PROTOTYPES
-void atMega2560T3(int prescalar, int outCompareRegister);
+void atMega2560_T3(int prescalar, int outCompareRegister);
 	// To set the prescalar of the timer enter one of the following values.
 	// If you enter a number not included, the function defaults to one.
 	// Optional prescalars: 1, 8, 64, 256, 1024
 	// The outCompareRegister is the number that the timer will count to 
-	// before the ISR is run
+	// before the ISR is run. T3 is a 16 bit timer.
 	// For timer 3, using the following line of code to attach the ISR
 	// ISR(TIMER3_COMPA_vect){ // put interrupt code here }
 
-void atMega2560T3(int prescalar, int outCompareRegister);
+void atMega2560_T3(int prescalar, int outCompareRegister);
 	// To set the prescalar of the timer enter one of the following values.
 	// If you enter a number not included, the function defaults to one.
 	// Optional prescalars: 1, 8, 64, 256, 1024
 	// The outCompareRegister is the number that the timer will count to 
-	// before the ISR is run
+	// before the ISR is run. T4 is a 16 bit timer.
 	// For timer 4, using the following line of code to attach the ISR
 	// ISR(TIMER4_COMPA_vect){ // put interrupt code here }
+
+void atMega328P_T1(int prescalar, int outCompareRegister);
+	// NOTE: This timer can be run at 16 MHz or 8 Mhz, look at your hardware!
+	// To set the prescalar of the timer enter one of the following values.
+	// If you enter a number not included, the function defaults to one.
+	// Optional prescalars: 1, 8, 64, 256, 1024
+	// The outCompareRegister is the number that the timer will count to 
+	// before the ISR is run. T1 is a 16 bit timer.
+	// For timer 1, using the following line of code to attach the ISR
+	// ISR(TIMER1_COMPA_vect){ // put interrupt code here }
 
 #endif
