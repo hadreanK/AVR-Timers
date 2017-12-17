19d0e2c39b6a1d2c2b88625e8be4c85b2b04b682
Adrian Kirn <kirnadrian@gmail.com>
2017-12-17 08:36:56 -0800

    Fixed Prescalar
    Changed the prescalar from what you want the prescalar to be to the three bit choices that make the prescalar

--- avrTimers.cpp
+++ avrTimers.cpp
@@ -1,5 +1,5 @@
-/* 
-This file is to help the LED_Clock arduino file to control my 
+/*
+This file is to help the LED_Clock arduino file to control my
 LED display I jacked from that sony clock
 */
 
@@ -8,27 +8,27 @@ LED display I jacked from that sony clock
 
 // INCLUDES
 	#include "Arduino.h"
-	
+
 // Functions
 void atMega2560_T3( int prescalar, int outCompareRegister){
 		// Set the type of timer
 		TCCR3A = 0x00; // Want bits 1:0 to be 00, they're WGM3 1:0, and everything else too
 		// Set the prescalar
-		int clkSelBit;
-		switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
-			case 8:
-				clkSelBit = 0b010;
-			case 64:
-				clkSelBit = 0b011;
-			case 256:
-				clkSelBit = 0b100;
-			case 1024:
-				clkSelBit = 0b101;
-			default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
-				clkSelBit = 0b001;
-			}// switch
+		// int clkSelBit;
+		// switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
+		// 	case 8:
+		// 		clkSelBit = 0b010;
+		// 	case 64:
+		// 		clkSelBit = 0b011;
+		// 	case 256:
+		// 		clkSelBit = 0b100;
+		// 	case 1024:
+		// 		clkSelBit = 0b101;
+		// 	default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
+		// 		clkSelBit = 0b001;
+		// 	}// switch
 		// set prescalar and other settings
-		TCCR3B = 0x08 + clkSelBit; // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
+		TCCR3B = 0b1000 + (prescalar&0b111); // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
 		// Bits 4:3 are WGM3 3:2, so we want them to be 0b01, b/c WGMn3:0 = 0b0100, table 17-2 page 145,
 		//   because this is CTC mode which clears the counter once it gets to OCR3A
 		// Bits 2:0 are the prescalar, coming from clkSelBit
@@ -40,21 +40,21 @@ void atMega2560_T4( int prescalar, int outCompareRegister){
 		// Set the type of timer
 		TCCR4A = 0x00; // Want bits 1:0 to be 00, they're WGM3 1:0, and everything else too
 		// Set the prescalar
-		int clkSelBit;
-		switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
-			case 8:
-				clkSelBit = 0b010;
-			case 64:
-				clkSelBit = 0b011;
-			case 256:
-				clkSelBit = 0b100;
-			case 1024:
-				clkSelBit = 0b101;
-			default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
-				clkSelBit = 0b001;
-			}// switch
+		// int clkSelBit;
+		// switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
+		// 	case 8:
+		// 		clkSelBit = 0b010;
+		// 	case 64:
+		// 		clkSelBit = 0b011;
+		// 	case 256:
+		// 		clkSelBit = 0b100;
+		// 	case 1024:
+		// 		clkSelBit = 0b101;
+		// 	default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
+		// 		clkSelBit = 0b001;
+		// 	}// switch
 		// set prescalar and other settings
-		TCCR4B = 0x08 + clkSelBit; // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
+		TCCR4B = 0b1000 + (prescalar&0b111); // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
 		// Bits 4:3 are WGM3 3:2, so we want them to be 0b01, b/c WGMn3:0 = 0b0100, table 17-2 page 145,
 		//   because this is CTC mode which clears the counter once it gets to OCR3A
 		// Bits 2:0 are the prescalar, coming from clkSelBit
@@ -66,21 +66,21 @@ void atMega328P_T1(int prescalar, int outCompareRegister){
 		// Set the type of timer
 		TCCR1A = 0x00; // Want bits 1:0 to be 00, they're WGM3 1:0, and everything else too
 		// Set the prescalar
-		int clkSelBit;
-		switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
-			case 8:
-				clkSelBit = 0b010;
-			case 64:
-				clkSelBit = 0b011;
-			case 256:
-				clkSelBit = 0b100;
-			case 1024:
-				clkSelBit = 0b101;
-			default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
-				clkSelBit = 0b001;
-			}// switch
+		// int clkSelBit;
+		// switch (prescalar){ // get the prescalar values from the table 17-6 on p 157
+		// 	case 8:
+		// 		clkSelBit = 0b010;
+		// 	case 64:
+		// 		clkSelBit = 0b011;
+		// 	case 256:
+		// 		clkSelBit = 0b100;
+		// 	case 1024:
+		// 		clkSelBit = 0b101;
+		// 	default: // if the user didn't enter 8, 64, 256, 1024, set default of 1
+		// 		clkSelBit = 0b001;
+		// 	}// switch
 		// set prescalar and other settings
-		TCCR1B = 0x08 + clkSelBit; // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
+		TCCR1B = 0b1000 + (prescalar&0b111); // Bits 7:6 have to do with input capture, 5 is reserved, 4:3 are waveform generation
 		// Bits 4:3 are WGM3 3:2, so we want them to be 0b01, b/c WGMn3:0 = 0b0100, table 20-6 page 171,
 		//   because this is CTC mode which clears the counter once it gets to OCR3A
 		// Bits 2:0 are the prescalar, coming from clkSelBit
--- avrTimers.h
+++ avrTimers.h
@@ -7,18 +7,26 @@
 					// FUNCTION PROTOTYPES
 void atMega2560_T3(int prescalar, int outCompareRegister);
 	// To set the prescalar of the timer enter one of the following values.
-	// If you enter a number not included, the function defaults to one.
-	// Optional prescalars: 1, 8, 64, 256, 1024
-	// The outCompareRegister is the number that the timer will count to 
+	// Optional prescalars:
+        // For 1, use prescalar 0b001
+        // For 8, use prescalar 0b010
+        // For 64, use prescalar 0b011
+        // For 256, use prescalar 0b100
+        // For 1024, use prescalar 0b101
+	// The outCompareRegister is the number that the timer will count to
 	// before the ISR is run. T3 is a 16 bit timer.
 	// For timer 3, using the following line of code to attach the ISR
 	// ISR(TIMER3_COMPA_vect){ // put interrupt code here }
 
-void atMega2560_T3(int prescalar, int outCompareRegister);
+void atMega2560_T4(int prescalar, int outCompareRegister);
 	// To set the prescalar of the timer enter one of the following values.
-	// If you enter a number not included, the function defaults to one.
-	// Optional prescalars: 1, 8, 64, 256, 1024
-	// The outCompareRegister is the number that the timer will count to 
+    // Optional prescalars:
+        // For 1, use prescalar 0b001
+        // For 8, use prescalar 0b010
+        // For 64, use prescalar 0b011
+        // For 256, use prescalar 0b100
+        // For 1024, use prescalar 0b101
+	// The outCompareRegister is the number that the timer will count to
 	// before the ISR is run. T4 is a 16 bit timer.
 	// For timer 4, using the following line of code to attach the ISR
 	// ISR(TIMER4_COMPA_vect){ // put interrupt code here }
@@ -26,9 +34,13 @@ void atMega2560_T3(int prescalar, int outCompareRegister);
 void atMega328P_T1(int prescalar, int outCompareRegister);
 	// NOTE: This timer can be run at 16 MHz or 8 Mhz, look at your hardware!
 	// To set the prescalar of the timer enter one of the following values.
-	// If you enter a number not included, the function defaults to one.
-	// Optional prescalars: 1, 8, 64, 256, 1024
-	// The outCompareRegister is the number that the timer will count to 
+    // Optional prescalars:
+        // For 1, use prescalar 0b001
+        // For 8, use prescalar 0b010
+        // For 64, use prescalar 0b011
+        // For 256, use prescalar 0b100
+        // For 1024, use prescalar 0b101
+	// The outCompareRegister is the number that the timer will count to
 	// before the ISR is run. T1 is a 16 bit timer.
 	// For timer 1, using the following line of code to attach the ISR
 	// ISR(TIMER1_COMPA_vect){ // put interrupt code here }
